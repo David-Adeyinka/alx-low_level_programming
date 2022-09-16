a7bb9fed052bd73ce7ce00e345c67d39e0584c42
David-Adeyinka <adeyinkad46@gmail.com>
2022-09-15 19:16:22 -0700

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - returns the function of its parameters
+ * @a: int type number
+ * @b: int tpye number 
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+return (a*b);
+}
