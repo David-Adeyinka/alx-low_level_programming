760699cd9ea28e00df9d89cf3d8bad11304ada72
David-Adeyinka <adeyinkad46@gmail.com>
2022-09-15 22:27:17 -0700

    some more fixing

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,10 @@
 /**
  * mul - returns the function of its parameters
  * @a: int type number
- * @b: int tpye number 
+ * @b: int tpye number
  * Return: 0
  */
 int mul(int a, int b)
 {
-return (a*b);
+return (a * b);
 }
